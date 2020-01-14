101d1c86040197f5b3c6ea54adcbab3ccce64bf8
raistlinJ <raistlinJ@users.noreply.github.com>
2020-01-14 14:30:57 -0700

    Changed Engine test cases to reflect standalone test

--- src/main/python/engine/Engine.py
+++ src/main/python/engine/Engine.py
@@ -313,104 +313,126 @@ if __name__ == "__main__":
     res = e.execute("engine status ")
 
 ###VMManage tests
-    sleep(5)
     #Check status without refresh
+    logging.debug("VM-Manage Status of defaulta without refresh" + str(res))
     res = e.execute("vm-manage vmstatus defaulta")
-    logging.debug("VM-Manage Status of ubuntu-core4.7: " + str(res))
+    res = e.execute("vm-manage mgrstatus")
+    logging.debug("Returned: " + str(res))
+    while res["readStatus"] != VMManage.MANAGER_IDLE and res["readStatus"] != VMManage.MANAGER_UNKNOWN:
+        sleep(1)
+        logging.debug("Waiting for vmstatus to complete...")
+        res = e.execute("vm-manage mgrstatus")
+        logging.debug("Returned: " + str(res))
+    logging.debug("VM-Manage vmstatus complete.")
     
     #Refresh
     sleep(5)
-    res = e.execute("vm-manage refresh")
-    logging.debug("Refreshing" + str(res))
+    res = e.execute("vm-manage refresh")    
+    res = e.execute("vm-manage mgrstatus")
+    logging.debug("Returned: " + str(res))
+    while res["readStatus"] != VMManage.MANAGER_IDLE:
+        sleep(1)
+        logging.debug("Waiting for vmrefresh to complete...")
+        res = e.execute("vm-manage mgrstatus")
+        logging.debug("Returned: " + str(res))
+    logging.debug("VM-Manage vmstatus complete.")
 
     #Check status after refresh
+    sleep(5)
     res = e.execute("vm-manage vmstatus defaulta")
-    logging.debug("VM-Manage Status of ubuntu-core4.7: " + str(res))
+    logging.debug("VM-Manage Status of defaulta: " + str(res))
+    res = e.execute("vm-manage mgrstatus")
+    logging.debug("Returned: " + str(res))
+    while res["readStatus"] != VMManage.MANAGER_IDLE:
+        sleep(1)
+        logging.debug("Waiting for vmstatus to complete...")
+        res = e.execute("vm-manage mgrstatus")
+        logging.debug("Returned: " + str(res))
+    logging.debug("VM-Manage vmstatus complete.")
 
 ###Packager tests
-    #e.execute(sys.argv[1:])
-    e.execute("packager status")
-    
     ###import
-    logging.debug("Importing VM: " + str("samples\sample.res"))
+    sleep(5)
+    logging.debug("Importing RES file: " + str("samples\sample.res"))
     e.execute("packager import \"samples\sample.res\"")
     res = e.execute("packager status")
     logging.debug("Returned: " + str(res))
-    exit()
-
     while res["writeStatus"] != PackageManageVBox.PACKAGE_MANAGE_COMPLETE:
         sleep(1)
         logging.debug("Waiting for package import to complete...")
         res = e.execute("packager status")
         logging.debug("Returned: " + str(res))
     logging.debug("Package import complete.")
-    
-    e.execute("packager export sample \"sample\myresfile.res\"")
+
+    ###export
+    sleep(5)
+    logging.debug("Exporting experiment named: sample to " + "exportedtestwithspaces")
+    e.execute("packager export sample \"exported\sample with space\"")
     res = e.execute("packager status")
     while res["writeStatus"] != PackageManageVBox.PACKAGE_MANAGE_COMPLETE:
         sleep(1)
         logging.debug("Waiting for package export to complete...")
         res = e.execute("packager status")
-    logging.debug("Package export complete.") 
+    logging.debug("Package export complete.")    
     
-###Connection tests
-    # sleep(60)#alternative, check status until packager is complete and idle
-    e.execute("conns status")
-    e.execute("conns create sample")
+# ###Connection tests
+#     # sleep(60)#alternative, check status until packager is complete and idle
+#     e.execute("conns status")
+#     e.execute("conns create sample")
 
-    # sleep(10) #alternative, check status until connection manager is complete and idle
-    e.execute("conns status")
-    e.execute("conns remove sample")
+#     # sleep(10) #alternative, check status until connection manager is complete and idle
+#     e.execute("conns status")
+#     e.execute("conns remove sample")
     
-    # sleep(10) #alternative, check status until connection manager is complete and idle
-    e.execute("conns status")
-    e.execute("conns open sample 1 1")
+#     # sleep(10) #alternative, check status until connection manager is complete and idle
+#     e.execute("conns status")
+#     e.execute("conns open sample 1 1")
 
-    #####---Create Experiment Test#####
-    logging.info("Starting Experiment")
-    e.execute("experiment create sample")
-    res = e.execute("experiment status")
-    logging.debug("Waiting for experiment create to complete...")
-    while res["writeStatus"] != ExperimentManageVBox.EXPERIMENT_MANAGE_COMPLETE:
-        sleep(1)
-        logging.debug("Waiting for experiment create to complete...")
-        res = e.execute("experiment status")
-    logging.debug("Experiment create complete.")    
+#     #####---Create Experiment Test#####
+#     logging.info("Starting Experiment")
+#     e.execute("experiment create sample")
+#     res = e.execute("experiment status")
+#     logging.debug("Waiting for experiment create to complete...")
+#     while res["writeStatus"] != ExperimentManageVBox.EXPERIMENT_MANAGE_COMPLETE:
+#         sleep(1)
+#         logging.debug("Waiting for experiment create to complete...")
+#         res = e.execute("experiment status")
+#     logging.debug("Experiment create complete.")    
 
-    #####---Start Experiment Test#####
-    logging.info("Starting Experiment")
-    e.execute("experiment start sample")
-    res = e.execute("experiment status")
-    logging.debug("Waiting for experiment start to complete...")
-    while res["writeStatus"] != ExperimentManageVBox.EXPERIMENT_MANAGE_COMPLETE:
-        sleep(1)
-        logging.debug("Waiting for experiment start to complete...")
-        res = e.execute("experiment status")
-    logging.debug("Experiment start complete.")    
+#     #####---Start Experiment Test#####
+#     logging.info("Starting Experiment")
+#     e.execute("experiment start sample")
+#     res = e.execute("experiment status")
+#     logging.debug("Waiting for experiment start to complete...")
+#     while res["writeStatus"] != ExperimentManageVBox.EXPERIMENT_MANAGE_COMPLETE:
+#         sleep(1)
+#         logging.debug("Waiting for experiment start to complete...")
+#         res = e.execute("experiment status")
+#     logging.debug("Experiment start complete.")    
 
-    #####---Stop Experiment Test#####
-    sleep(5)
-    logging.info("Stopping Experiment")
-    e.execute("experiment stop sample")
-    res = e.execute("experiment status")
-    logging.debug("Waiting for experiment stop to complete...")
-    while res["writeStatus"] != ExperimentManageVBox.EXPERIMENT_MANAGE_COMPLETE:
-        sleep(1)
-        logging.debug("Waiting for experiment stop to complete...")
-        res = e.execute("experiment status")
-    logging.debug("Experiment stop complete.")    
+#     #####---Stop Experiment Test#####
+#     sleep(5)
+#     logging.info("Stopping Experiment")
+#     e.execute("experiment stop sample")
+#     res = e.execute("experiment status")
+#     logging.debug("Waiting for experiment stop to complete...")
+#     while res["writeStatus"] != ExperimentManageVBox.EXPERIMENT_MANAGE_COMPLETE:
+#         sleep(1)
+#         logging.debug("Waiting for experiment stop to complete...")
+#         res = e.execute("experiment status")
+#     logging.debug("Experiment stop complete.")    
 
-    #####---Remove Experiment Test#####
-    sleep(5)
-    logging.info("Remove Experiment")
-    e.execute("experiment remove sample")
-    res = e.execute("experiment status")
-    logging.debug("Waiting for experiment remove to complete...")
-    while res["writeStatus"] != ExperimentManageVBox.EXPERIMENT_MANAGE_COMPLETE:
-        sleep(1)
-        logging.debug("Waiting for experiment remove to complete...")
-        res = e.execute("experiment status")
-    logging.debug("Experiment remove complete.")    
+#     #####---Remove Experiment Test#####
+#     sleep(5)
+#     logging.info("Remove Experiment")
+#     e.execute("experiment remove sample")
+#     res = e.execute("experiment status")
+#     logging.debug("Waiting for experiment remove to complete...")
+#     while res["writeStatus"] != ExperimentManageVBox.EXPERIMENT_MANAGE_COMPLETE:
+#         sleep(1)
+#         logging.debug("Waiting for experiment remove to complete...")
+#         res = e.execute("experiment status")
+#     logging.debug("Experiment remove complete.")    
 
-    sleep(3) #allow some time for observation
-    #quit
+#     sleep(3) #allow some time for observation
+#     #quit
