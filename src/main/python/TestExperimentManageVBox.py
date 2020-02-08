531be609ac4a995b8a31debc7922fd601505a8ad
raistlinJ <raistlinJ@users.noreply.github.com>
2020-02-08 12:05:37 -0700

    restored all tests

--- src/main/python/TestExperimentManageVBox.py
+++ src/main/python/TestExperimentManageVBox.py
@@ -15,41 +15,40 @@ if __name__ == "__main__":
 
     logging.debug("Instantiating Engine")
     e = ExperimentManageVBox(initializeVMManage=True)
-
-    #####---Create Experiment Test#####
+    ####---Create Experiment Test#####
     logging.info("Creating Experiment")
     e.createExperiment("sample")
     result = e.getExperimentManageStatus()["writeStatus"]
     while result != e.EXPERIMENT_MANAGE_COMPLETE:
-        time.sleep(1)
+        time.sleep(.1)
         logging.debug("Waiting for experiment create to complete...")
         result = e.getExperimentManageStatus()["writeStatus"]
     
-    # #####---Start Experiment Test#####
-    # logging.info("Starting Experiment")
-    # e.startExperiment("sample")
-    # while e.getExperimentManageStatus()["writeStatus"] != e.EXPERIMENT_MANAGE_COMPLETE:
-    #     time.sleep(.1)
-    #     logging.debug("Waiting for experiment start to complete...")
-    # logging.debug("Experiment start complete.")    
+    #####---Start Experiment Test#####
+    logging.info("Starting Experiment")
+    e.startExperiment("sample")
+    while e.getExperimentManageStatus()["writeStatus"] != e.EXPERIMENT_MANAGE_COMPLETE:
+        time.sleep(.1)
+        logging.debug("Waiting for experiment start to complete...")
+    logging.debug("Experiment start complete.")    
 
-    # #####---Stop Experiment Test#####
-    # logging.info("Stopping Experiment")
-    # e.stopExperiment("sample")
-    # while e.getExperimentManageStatus()["writeStatus"] != e.EXPERIMENT_MANAGE_COMPLETE:
-    #     time.sleep(.1)
-    #     logging.debug("Waiting for experiment stop to complete...")
-    # logging.debug("Experiment stop complete.")    
+    #####---Stop Experiment Test#####
+    logging.info("Stopping Experiment")
+    e.stopExperiment("sample")
+    while e.getExperimentManageStatus()["writeStatus"] != e.EXPERIMENT_MANAGE_COMPLETE:
+        time.sleep(.1)
+        logging.debug("Waiting for experiment stop to complete...")
+    logging.debug("Experiment stop complete.")    
    
-    # #####---Restore Experiment Test#####
-    # logging.info("Stopping Experiment")
-    # e.restoreExperiment("sample")
-    # while e.getExperimentManageStatus()["writeStatus"] != e.EXPERIMENT_MANAGE_COMPLETE:
-    #     time.sleep(.1)
-    #     logging.debug("Waiting for experiment stop to complete...")
-    # logging.debug("Experiment stop complete.")
+    #####---Restore Experiment Test#####
+    logging.info("Stopping Experiment")
+    e.restoreExperiment("sample")
+    while e.getExperimentManageStatus()["writeStatus"] != e.EXPERIMENT_MANAGE_COMPLETE:
+        time.sleep(.1)
+        logging.debug("Waiting for experiment stop to complete...")
+    logging.debug("Experiment stop complete.")
 
-    ####---Remove Experiment Test#####
+    # #####---Remove Experiment Test#####
     logging.info("Removing Experiment")
     e.removeExperiment("sample")
     result = e.getExperimentManageStatus()["writeStatus"]
@@ -57,3 +56,5 @@ if __name__ == "__main__":
         time.sleep(.1)
         logging.debug("Waiting for experiment create to complete...")
         result = e.getExperimentManageStatus()["writeStatus"]
+
+
